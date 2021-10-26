4cc6102d1d1b9ce799d656f32c2fda02da2db515
Antonio Marangi <antonio.marangi@alba-robot.com>
2021-10-26 16:08:19 +0200

    first commit

---
+++ core.py
@@ -0,0 +1,151 @@
+import time
+import requests
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.select import Select
+from selenium.common.exceptions import NoSuchElementException
+from mytelegram import bot
+from selenium.webdriver.common.by import By
+import re
+import warnings
+from datetime import datetime
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+hless = True
+id = "1"
+DepartureStation = "Torino ( Tutte Le Stazioni )"
+ArrivalStation = "Bari ( Tutte Le Stazioni )"
+day=23
+month = 12
+year=2021
+hr = "08"
+restTime=0 #seconds
+cycleTime=60 #seconds
+notification=60 #minutes
+cnt=0
+
+def init():
+    print("Initializing...")
+    options = webdriver.FirefoxOptions()
+    #options = webdriver.ChromeOptions()
+    if hless:
+        options.add_argument('--headless')
+    driver = webdriver.Firefox(options=options)
+    #options.add_argument('window-size=1200x600')
+    return driver
+def calculateMonthDif():
+    if datetime.now().year==year:
+        return month-datetime.now().month
+    if datetime.now().year<year:
+        return 12-datetime.now().month+month
+def searchTrain(driver):
+    driver.get("https://www.trenitalia.com/")
+    assert "Trenitalia" in driver.title
+    depStat = driver.find_element_by_name("departureStation")
+    print("Departure Station found")
+    depStat.clear()
+    depStat.send_keys(DepartureStation)
+    depStat.send_keys(Keys.RETURN)
+    arrStat = driver.find_element_by_name("arrivalStation")
+    print("Arrival Station found")
+    arrStat.clear()
+    arrStat.send_keys(ArrivalStation)
+    arrStat.send_keys(Keys.RETURN)
+    calendar = driver.find_element_by_name("departureDateVISIBLE")
+    print("Date found")
+    calendar.click()
+    dif=calculateMonthDif()
+    for i in range(0,dif-1):
+        nxtMonthButt = driver.find_element(By.ID,"ui-datepicker-div").find_element(By.CLASS_NAME,"ui-datepicker-group-last").find_element(By.CLASS_NAME,"ui-datepicker-header").find_element(By.CLASS_NAME,"ui-datepicker-next")
+        print("Next month found")
+        nxtMonthButt.click()
+    date = driver.find_element(By.ID,"ui-datepicker-div").find_element(By.CLASS_NAME,"ui-datepicker-group-last").find_element(By.CLASS_NAME,"ui-datepicker-calendar").find_elements(By.TAG_NAME,"td")
+    for dat in date:
+        if int(dat.text)==day:
+            print("Date found")
+            dat.click()
+            break
+    depTime = driver.find_element_by_id("biglietti_ora_p")
+    print("Time found")
+    Select(depTime).select_by_value(hr)
+    passenger = driver.find_element_by_id("biglietti_adulti")
+    print("Passengers found")
+    passenger.click()
+    plus = driver.find_element_by_id('addAdult')
+    print("Number of passengers found")
+    plus.click()
+    go = driver.find_element_by_xpath("/html/body/div[5]/form/div/div[3]/div[2]/div[6]/button")
+    print("Search found")
+    go.click()
+    return 0
+
+def getTrainList(driver):
+    l =[]
+    i=0
+    while True:
+        try:
+            l.append(driver.find_element(By.ID, 'travelSolution'+str(i)))
+            i+=1
+        except NoSuchElementException:
+            print("Solutions extracted: "+str(i-1))
+            break
+    if i!=0:
+        return l
+    else:
+        return -1
+
+def getTimes(solutions):
+    d=[]
+    for train in solutions:
+        index = str(solutions.index(train))
+        try:
+            print("Searching times for solution "+index)
+            hr=train.find_element(By.ID,"deptTime_"+index).get_attribute("value")
+            ch=train.find_element(By.CLASS_NAME,"table-solution-hover").find_element(By.CLASS_NAME,"solutionRow").find_element(By.CLASS_NAME,"trainOffer").find_element(By.CLASS_NAME,"descr").text
+            d.append(str(hr)+"_"+str(ch))
+        except NoSuchElementException:
+            print("Error in getting times or changes")
+            return -1
+    for dd in d:
+        if "*" in dd:
+            d.remove(dd)
+    return d
+
+def inform(mybot,message):
+    mybot.send(message)
+
+if __name__ == "__main__":
+    mybot = bot("AntonioMarangi"+id)
+    lastTimes=0
+    while True:
+        try:  
+            t = time.time()     
+            driver = init()
+            print("Inizialized")
+            res = searchTrain(driver)
+            if res!=0:
+                raise Exception
+            Trains = getTrainList(driver)
+            if Trains==-1:
+                raise Exception
+            Times = getTimes(Trains)
+            if Times==-1:
+                raise Exception
+            if len(Times)!=lastTimes:
+                lastTimes=len(Times)
+                inform(mybot,str(Times))
+                print("Informing")
+            if cnt==notification:
+                inform(mybot,str(Times))
+                cnt=0
+                print("Informing")
+            driver.close()
+            elapsed = time.time()-t
+            print("Elapsed: "+str(elapsed))
+            cnt+=1
+            print("Iteration: "+str(cnt))
+            time.sleep(cycleTime-elapsed)
+        except Exception:
+            print("error: END")
+            time.sleep(restTime)
+
+
---
+++ mytelegram.py
@@ -0,0 +1,47 @@
+# importing all required libraries
+import telebot
+from telethon.sync import TelegramClient
+from telethon.tl.types import InputPeerUser, InputPeerChannel
+from telethon import TelegramClient, sync, events
+class bot():
+    def __init__(self,id):
+        # get your api_id, api_hash, token
+        # from telegram as described above
+        self.api_id = '8758081'
+        self.api_hash = '3988872d49be46339271455e359b29ba'
+        self.message = "Working..."
+
+        # your phone number
+        self.phone = '00393464768609'
+
+        # creating a telegram session and assigning
+        # it to a variable client
+        self.client = TelegramClient(id, self.api_id, self.api_hash)
+
+        # connecting and building the session
+        self.client.connect()
+
+        # in case of script ran first time it will
+        # ask either to input token or otp sent to
+        # number or sent or your telegram id
+        if not self.client.is_user_authorized():
+
+            self.client.send_code_request(self.phone)
+            
+            # signing in the client
+            self.client.sign_in(self.phone, input('Enter the code: '))
+
+    def send(self, mess):
+        try:
+            # receiver user_id and access_hash, use
+            # my user_id and access_hash for reference
+            #self.client.send_message("+393288135320",mess)
+            #self.client.send_message("+393288135185",mess)
+            self.client.send_message("+393464768609",mess)
+        except Exception as e:           
+            # there may be many error coming in while like peer
+            # error, wrong access_hash, flood_error, etc
+            print(e)
+
+        # disconnecting the telegram session
+        #client.disconnect()
